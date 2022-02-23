0a988139982f4672592f9f56c31bf2c35543883d
Thomas Ruppeiner <truppeiner@gmail.com>
2022-02-22 18:07:02 -0600

    resolved issue #2

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,27 +5,44 @@ var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 //function to dynamically create task items (task-handler)
 
-var createTaskHandler = function (event) {
+var taskFormHandler = function (event) {
     event.preventDefault();
         var taskNameInput = document.querySelector("input[name='task-name']").value;
         var taskTypeInput = document.querySelector("select[name='task-type']").value;
 
-        //CREATE LIST ITEM
-        var listItemEl = document.createElement("li");
-        listItemEl.className = "task-item";
+        //CHECK IF INPUT VALUES ARE EMPTY STRINGS
+        if (!taskNameInput || !taskTypeInput) {
+            alert("You need to fill out the task form!");
+            return false;
+        }
+        formEl.reset();
+        //PACKAGE DATA AS OBJECT
+        var taskDataObj = {
+            name: taskNameInput,
+            type: taskTypeInput
+        };
 
-        //CREATE DIV TO HOLD TASK INFO AND ADD TO LIST ITEM
-        var taskInfoEl = document.createElement("div");
-
-        // GIVE IT A CLASS NAME
-        taskInfoEl.className = "task-info";
-        
-        // ADD HTML CONTENT TO DIV
-        taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
-        listItemEl.appendChild(taskInfoEl);
-
-        // ADD ENTIRE LIST ITEM TO LIST
-        tasksToDoEl.appendChild(listItemEl);
+        //SEND IT AS AN ARGUMENT TO CREATETASKEL
+        createTaskEl(taskDataObj);
 };
 
-formEl.addEventListener("submit", createTaskHandler);
+var createTaskEl = function(taskDataObj) {
+    //CREATE LIST ITEM
+    var listItemEl = document.createElement("li");
+     listItemEl.className = "task-item";
+    
+    //CREATE DIV TO HOLD TASK INFO AND ADD TO LIST ITEM
+    var taskInfoEl = document.createElement("div");
+    
+    // GIVE IT A CLASS NAME
+    taskInfoEl.className = "task-info";
+    
+    // ADD HTML CONTENT TO DIV
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
+    listItemEl.appendChild(taskInfoEl);
+    
+    // ADD ENTIRE LIST ITEM TO LIST
+    tasksToDoEl.appendChild(listItemEl);
+};
+
+formEl.addEventListener("submit", taskFormHandler);
